bce9f11311038786d19261d505512e62344a152e
Robert Deak <robert.deak@gmail.com>
2026-03-16 13:24:45 +0100

    fix: json import

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,21 @@
 import { defineConfig } from 'astro/config';
 import react from '@astrojs/react';
-import tailwindv4 from '@tailwindcss/vite';
+
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
   site: 'https://rdeak.github.io',
   base: '/novena-tacker-pwa',
   output: 'static',
+  trailingSlash: 'always',
   integrations: [
     react(),
   ],
   vite: {
-    plugins: [tailwindv4()],
     build: {
       cssMinify: 'lightningcss',
-    }
-  }
+    },
+
+    plugins: [tailwindcss()],
+  },
 });
